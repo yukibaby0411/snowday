429d2a009a2d85c31583378abcaf645c711455b3
yukibaby0411 <yuefei.12@qq.com>
2017-01-11 03:15:29 +0000

    huanjing over

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,15 @@
         <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
 
         <style>
             html, body {
                 height: 100%;
+                -webkit-user-select:none;
+                -moz-user-select:none;
+                -ms-user-select:none;
+                user-select:none;
             }
 
             body {
@@ -28,17 +33,36 @@
             .content {
                 text-align: center;
                 display: inline-block;
+                font-size: 72px;
+                cursor: default;
             }
 
             .title {
-                font-size: 96px;
+                display: none;
             }
         </style>
+
+        <script src="/js/app.js"></script>
+        <script>
+            $(document).ready(function () {
+                setTimeout("$('.title1').fadeOut(1000)",750);
+                setTimeout("$('.title2').fadeIn(1000)",1750);
+                setTimeout("$('.title2').fadeOut(1000)",3500);
+                setTimeout("$('.title3').fadeIn(1000)",4500);
+                setTimeout("$('.title3').fadeOut(1000)",6250);
+                setTimeout("$('.title4').fadeIn(1000)",7250);
+                setTimeout("$('.title4').fadeOut(1000)",9000);
+                setTimeout("window.location.href='http://snowday.app'",9500);
+            });
+        </script>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title1">永远相信...</div>
+                <div class="title title2">美好的事情即将发生</div>
+                <div class="title title3">欢迎来到优一兔社区</div>
+                <div class="title title4">即将开启你的新奇之旅</div>
             </div>
         </div>
     </body>
