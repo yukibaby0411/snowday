b685fe4b70b36c51a0e5f1a6b9b1dc720af3c8e7
yukibaby0411 <yuefei.12@qq.com>
2017-01-12 03:30:05 +0000

    home submit over

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,25 @@
+<style>
+    .nav_line{ cursor: default;}
+    .navbar-left{ margin-left: 25px;}
+</style>
 <header class="navbar navbar-fixed-top navbar-inverse">
     <div class="container" style="width: 1100px; padding: 0;">
         <a href="{{ route('home') }}" id="logo">u1tu social</a>
+        <nav>
+            <ul class="nav navbar-nav navbar-left">
+                <li><a href="">社区</a></li>
+                <li><a href="javascript:void(0);" class="nav_line"> | </a></li>
+                <li><a href="">博客</a></li>
+                <li><a href="javascript:void(0);" class="nav_line"> | </a></li>
+                <li><a href="">资讯</a></li>
+                <li><a href="javascript:void(0);" class="nav_line"> | </a></li>
+                <li><a href="">Code</a></li>
+                <li><a href="javascript:void(0);" class="nav_line"> | </a></li>
+                <li><a href="">招聘</a></li>
+                <li><a href="javascript:void(0);" class="nav_line"> | </a></li>
+                <li><a href="">休闲</a></li>
+            </ul>
+        </nav>
         <nav>
             <ul class="nav navbar-nav navbar-right">
                 @if(1)
